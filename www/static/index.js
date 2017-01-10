5c6b12c96c6719d7c3ef975990c7de45a5063fa5
Jordan Jones <proxima.aust@gmail.com>
2017-01-09 21:27:49 -0500

    Bootstrap fonts added and minor fixes

--- www/static/index.js
+++ www/static/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App.js';
 
-import './dist/css/bootstrap-theme.min.css';
 import './js/chart/index.js';
+import './dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <App />,
