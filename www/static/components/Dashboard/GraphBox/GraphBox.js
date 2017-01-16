a53394d7b1a4cdc22fa90b0e8fb02b5442842181
Jordan Jones <proxima.aust@gmail.com>
2017-01-15 22:46:23 -0500

    GraphBox components added in

--- www/static/components/Dashboard/GraphBox/GraphBox.js
+++ www/static/components/Dashboard/GraphBox/GraphBox.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Col } from 'react-bootstrap';
+
+import BasicAreaChart from './BasicAreaChart/BasicAreaChart';
 
 class GraphBox extends Component{
   constructor() {
@@ -7,9 +10,16 @@ class GraphBox extends Component{
 
   render(){
     return (
-      <div>
-        <p>GraphBox</p>
-      </div>
+      <Col xs={12} md={12}>
+        <Col xs={12} sm={12} md={12} lg={6}>
+          <BasicAreaChart></BasicAreaChart>
+          <BasicAreaChart></BasicAreaChart>
+        </Col>
+        <Col xs={12} sm={12} md={12} lg={6}>
+          <BasicAreaChart></BasicAreaChart>
+          <BasicAreaChart></BasicAreaChart>
+        </Col>
+      </Col>
     )
   }
 }
