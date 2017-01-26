efe125d2451217093c8dfd824622977a805f4641
Jordan Jones <proxima.aust@gmail.com>
2017-01-26 01:26:29 -0500

    Navbar and styles updated

--- www/static/components/Navbar/Navbar.js
+++ www/static/components/Navbar/Navbar.js
@@ -1,7 +1,16 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router';
-import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+/**
+ * Component: Footer
+ * Purpose: Displays Footer Section of UI
+ * Properties: None
+ **/
 
+ //React and React Bootstrap imports
+import React, { Component } from 'react';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+//React-Router imports for..Routing
+import { Link } from 'react-router';
+
+//Component's style
 import './Navbar.less'
 
 class NavBar extends Component{
@@ -11,20 +20,22 @@ class NavBar extends Component{
 
   render() {
     return(
-      <Navbar>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#">Garnet</a>
-          </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
-          <NavItem><Link to="/hosts">Hosts</Link></NavItem>
-          <NavItem><Link to="/agents">Agents</Link></NavItem>
-          <NavItem><Link to="/settings">Settings</Link></NavItem>
-        </Nav>
-      </Navbar>
+      <div className="NavBar">
+        <ul>
+          <li>
+            <Link to="/hosts">Hosts</Link>
+          </li>
+          <li>
+            <Link to="/"><img src="./static/img/garnet_logo.png" style={{'height':'30px','marginTop':'-5px'}}></img></Link>
+          </li>
+          <li>
+            <Link to="/hosts">Agents</Link>
+          </li>
+        </ul>
+      </div>
     )
   }
 }
 
+//Exports class to Global namespace
 export default NavBar;
