f1e3591e4dd7f902f16e42a9c95ee849e4bec400
Jordan Jones <proxima.aust@gmail.com>
2017-01-26 01:28:48 -0500

    Bundle and entry updated

--- www/static/index.js
+++ www/static/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App.js';
 
-import './dist/css/bootstrap.min.css';
+import './dist/css/bootstrap.css';
 
 ReactDOM.render(
   <App />,
