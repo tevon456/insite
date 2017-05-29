e2f43b4f1365f23112b7a5d1b520b4f1a0e8d677
Jordan Jones <proxima.aust@gmail.com>
2017-05-29 09:42:56 -0500

    Moved frontend and association files to own folder

---
+++ static/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App.js';
+
+import './dist/css/bootstrap.css';
+
+ReactDOM.render(
+  <App />,
+   document.getElementById('root')
+ );
+
