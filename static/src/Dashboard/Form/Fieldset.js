96a40d81bcdafaff2be42c2ac89f0c9aae7793a0
Mardii-D-King <mardon_workspace@yahoo.com>
2017-05-31 11:15:23 +0000

    Completed the config form layout

---
+++ static/src/Dashboard/Form/Fieldset.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Fieldset extends Component {
+  render () {
+    return (
+      <fieldset>
+        <legend>main</legend>
+      </fieldset>
+    );
+  }
+}
+
+export default Fieldset;
--- static/src/Settings/Settings.js
+++ static/src/Settings/Settings.js
@@ -6,20 +6,21 @@
  **/
 
 //React and React Bootstrap imports
-import React, {Component, fieldset, legend} from 'react';
+import React, {Component, Text} from 'react';
 import {
   Col,
-  FormGroup,
+  Row,
   ControlLabel,
-  HelpBlock,
   FormControl,
+  Grid,
+  Panel,
 } from 'react-bootstrap';
 import Navbar from '../Navbar/Navbar.js';
 import Footer from '../Dashboard/Footer/Footer.js';
-import Textbox from '../Dashboard/Form/Textbox.js';
-import {render} from 'react-dom';
+//import Fieldset from '../Dashboard/Form/Fieldset.js';
+import nl2br from 'react-newline-to-break';
 
-class Settings extends React.Component {
+class Settings extends Component {
   constructor () {
     super ();
     this.state = {
@@ -28,72 +29,198 @@ class Settings extends React.Component {
   }
 
   render () {
+    let br2ln = '\n';
     return (
       <div>
         <Navbar />
 
         <h1>Settings</h1>
 
+        <div>
+          {nl2br (br2ln)}
+        </div>
         <form>
+          <h3>main</h3>
 
-          <FormGroup controlId="formBasicText">
+          <Grid>
+            <Row className="show-grid">
 
-            <fieldset>
-              <legend>main</legend>
+              <Col xs={8} md={6}>
+                <ControlLabel>Log File</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="log/garnet.log"
+                />
 
-              <ControlLabel>Log File</ControlLabel>
-              <FormControl
-                type="text"
-                value={this.state.value}
-                placeholder="log/garnet.log"
-              />
+                <ControlLabel>Log Level</ControlLabel>
+                <FormControl componentClass="select" placeholder="select">
+                  <option value="other">info</option>
+                </FormControl>
 
-              <ControlLabel>Log Level</ControlLabel>
-              <FormControl componentClass="select" placeholder="select">
-                <option value="other">info</option>
-              </FormControl>
+                <ControlLabel>agent_cache_directory</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="cache"
+                />
 
-              <ControlLabel>agent_cache_directory</ControlLabel>
-              <FormControl
-                type="text"
-                value={this.state.value}
-                placeholder="cache"
-              />
+                <ControlLabel>language</ControlLabel>
+                <FormControl componentClass="select" placeholder="select">
+                  <option value="other">en</option>
+                </FormControl>
 
-              <ControlLabel>language</ControlLabel>
-              <FormControl componentClass="select" placeholder="select">
-                <option value="other">en</option>
-              </FormControl>
+                <ControlLabel>interval</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="100"
+                />
 
-              <ControlLabel>interval</ControlLabel>
-              <FormControl
-                type="text"
-                value={this.state.value}
-                placeholder="100"
-              />
+                <ControlLabel>agent_subprocesses</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="10"
+                />
 
-              <ControlLabel>agent_subprocesses</ControlLabel>
-              <FormControl
-                type="text"
-                value={this.state.value}
-                placeholder="10"
-              />
+                <ControlLabel>listen_address</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="127.0.0.1"
+                />
 
-              <ControlLabel>listen_address</ControlLabel>
-              <FormControl
-                type="text"
-                value={this.state.value}
-                placeholder="127.0.0.1"
-              />
+                <ControlLabel>bind_port</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="5000"
+                />
+              </Col>
 
-              <ControlLabel>bind_port</ControlLabel>
-              <FormControl
-                type="text"
-                value={this.state.value}
-                placeholder="5000"
-              />
-            </fieldset>
-          </FormGroup>
+              <Col xs={8} md={6}>
+                <ControlLabel>Agent Name</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="_garnet"
+                />
+
+                <ControlLabel>Agent Enabled</ControlLabel>
+                <FormControl componentClass="select" placeholder="select">
+                  <option value="other">True</option>
+                  <option value="other">False</option>
+                </FormControl>
+
+                <ControlLabel>Agent Filename</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="bin/agents/_garnet.py"
+                />
+
+                <ControlLabel>Monitor Agent Pid</ControlLabel>
+                <FormControl componentClass="select" placeholder="select">
+                  <option value="other">True</option>
+                  <option value="other">False</option>
+                </FormControl>
+
+                <div>
+                  {nl2br (br2ln)}
+                </div>
+
+                <ControlLabel>Agent Name</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="Linux"
+                />
+
+                <ControlLabel>Agent Enabled</ControlLabel>
+                <FormControl componentClass="select" placeholder="select">
+                  <option value="other">False</option>
+                  <option value="other">True</option>
+                </FormControl>
+
+                <ControlLabel>Agent Filename</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="bin/agents/linux.py"
+                />
+
+                <ControlLabel>Agent Port</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="5001"
+                />
+
+                <ControlLabel>Monitor Agent Pid</ControlLabel>
+                <FormControl componentClass="select" placeholder="select">
+                  <option value="other">True</option>
+                  <option value="other">False</option>
+                </FormControl>
+
+                <ControlLabel>Agent Devicenames</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="192.168.1.234"
+                />
+
+                <div>
+                  {nl2br (br2ln)}
+                </div>
+
+                <ControlLabel>Agent Name</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="remote_linux_active"
+                />
+
+                <ControlLabel>Agent Enabled</ControlLabel>
+                <FormControl componentClass="select" placeholder="select">
+                  <option value="other">False</option>
+                  <option value="other">True</option>
+                </FormControl>
+
+                <ControlLabel>Agent Filename</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="bin/agents/remote_linux_active.py"
+                />
+
+                <div>
+                  {nl2br (br2ln)}
+                </div>
+
+                <ControlLabel>Agent Name</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="remote_linux_passive"
+                />
+
+                <ControlLabel>Agent Enabled</ControlLabel>
+                <FormControl componentClass="select" placeholder="select">
+                  <option value="other">False</option>
+                  <option value="other">True</option>
+                </FormControl>
+
+                <ControlLabel>Agent Filename</ControlLabel>
+                <FormControl
+                  type="text"
+                  value={this.state.value}
+                  placeholder="bin/agents/remote_linux_passive.py"
+                />
+
+              </Col>
+            </Row>
+          </Grid>
 
         </form>
 
