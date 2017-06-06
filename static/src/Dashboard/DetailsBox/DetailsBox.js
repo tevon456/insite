6438b0f33f6505874a3f750c5cf855f582bcc02d
Jordan Jones <proxima.aust@gmail.com>
2017-06-06 14:29:00 -0500

    Ran prettier, formatted js

--- static/src/Dashboard/DetailsBox/DetailsBox.js
+++ static/src/Dashboard/DetailsBox/DetailsBox.js
@@ -8,51 +8,50 @@
  **/
 
 //React and React Bootstrap imports
-import React, { Component } from 'react';
-import { Table, Panel, Col } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Table, Panel, Col } from "react-bootstrap";
 
 //Axios import, HTTP Promise library
-import axios from 'axios';
+import axios from "axios";
 
 //Component's style
-import './DetailsBox.less';
+import "./DetailsBox.less";
 
 class DetailsBox extends Component {
   constructor(props) {
     super(props);
-
   }
 
   render() {
     //Additional inline styles
-    var style= {
-      'textAlign':'center',
+    var style = {
+      textAlign: "center"
     };
 
     return (
       <Panel className="DetailsBox" style={style}>
         <Col sm={4} md={4}>
-          <img src="static/img/id-card.svg"></img>
+          <img src="img/id-card.svg" />
           <p><b>Host</b> {this.props.device}</p>
         </Col>
         <Col sm={4} md={4}>
-          <img src="static/img/monitor.svg"></img>
+          <img src="img/monitor.svg" />
           <p><b>Operating System</b> {this.props.system}</p>
         </Col>
         <Col sm={4} md={4}>
-          <img src="static/img/settings.svg"></img>
+          <img src="img/settings.svg" />
           <p><b>Version</b> {this.props.version}</p>
         </Col>
       </Panel>
-    )
+    );
   }
 }
 
 //Default properties of Component
 DetailsBox.defaultProps = {
-  device:"",
-  system:"",
-  version:""
-}
+  device: "",
+  system: "",
+  version: ""
+};
 //Exports Component to Global namespace
 export default DetailsBox;
--- static/src/Navbar/Navbar.js
+++ static/src/Navbar/Navbar.js
@@ -4,36 +4,38 @@
  * Properties: None
  **/
 
- //React and React Bootstrap imports
-import React, { Component } from 'react';
-import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+//React and React Bootstrap imports
+import React, { Component } from "react";
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
 //React-Router imports for..Routing
-import { Link } from 'react-router';
+import { Link } from "react-router";
 
 //Component's style
-import './Navbar.less'
+import "./Navbar.less";
 
-class NavBar extends Component{
+class NavBar extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return(
+    return (
       <div className="NavBar">
         <ul>
           <li>
             <Link to="/hosts"><b>Hosts</b></Link>
           </li>
           <li>
-            <Link to="/"><img src="./static/img/garnet_logo.png" style={{'height':'35px',}}></img></Link>
+            <Link to="/">
+              <img src="./img/garnet_logo.png" style={{ height: "35px" }} />
+            </Link>
           </li>
           <li>
             <Link to="/hosts"><b>Agents</b></Link>
           </li>
         </ul>
       </div>
-    )
+    );
   }
 }
 
