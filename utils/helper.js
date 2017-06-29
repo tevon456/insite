207962277a82d284d858ef88fb61c62e29e0e5d6
Mardii-D-King <mardon_workspace@yahoo.com>
2017-06-29 22:09:53 +0000

    Use pretty to format code structure.

--- utils/helper.js
+++ utils/helper.js
@@ -1,8 +1,7 @@
-const _ = require("lodash");
+const _ = require ('lodash');
 
-function _datapoint_labels(idx_device, idx_agent, labels){
-
-	/**
+function _datapoint_labels (idx_device, idx_agent, labels) {
+  /**
    * @param idx_device
    * @param idx-agent
    * @param labels
@@ -10,25 +9,23 @@ function _datapoint_labels(idx_device, idx_agent, labels){
    * @returns listings
    */
 
-   var listing = [];
+  var listing = [];
 
-   var datapoints = _get_datapoints(idx_agent, idx_device);
+  var datapoints = _get_datapoints (idx_agent, idx_device);
 
-   for (var data_dict of datapoints) {
-    
-    		var	agent_label = data_dict['agent_label'];
-        	var idx_datapoint = data_dict['idx_datapoint'];
+  for (var data_dict of datapoints) {
+    var agent_label = data_dict['agent_label'];
+    var idx_datapoint = data_dict['idx_datapoint'];
 
-        if(agent_label of labels)
-        {
-        	listing.append(idx_datapoint);
-        }
+    if (agent_label) of (labels);
+    {
+      listing.append (idx_datapoint);
     }
-
-    return listing;
+  }
+  return listing;
 }
 
-function _d3_converter(values, chart_values, agent_label) {
+function _d3_converter (values, chart_values, agent_label) {
   /**
    * @param values
    * @param chart_values
@@ -40,16 +37,15 @@ function _d3_converter(values, chart_values, agent_label) {
   var agent_values = [];
 
   //If chart values not empty
-  if (!_.isEmpty(chart_values)) {
-    for (var [timestamp, value] of chart_values.sorted()) {
-      chart_values.push({ timestamp: timestamp });
-      agent_values.push(value);
+  if (!_.isEmpty (chart_values)) {
+    for (var [timestamp, value] of chart_values.sorted ()) {
+      chart_values.push ({timestamp: timestamp});
+      agent_values.push (value);
     }
   } else {
-    for (var [dicts, agent_value] of _.zip(chart_values, agent_values)) {
+    for (var [dicts, agent_value] of _.zip (chart_values, agent_values)) {
       dicts[agent_label] = agent_value;
     }
     return chart_values;
   }
 }
-
