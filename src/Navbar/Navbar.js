215eafcc68381342ed07a2f77c6fa93042e979df
Jordan Jones <proxima.aust@gmail.com>
2017-07-29 19:19:57 -0500

    Ran prettier and added Data link

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -5,23 +5,25 @@
  **/
 
 //React and React Bootstrap imports
-import React, {Component} from 'react';
-import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+import React, { Component } from "react";
 //React-Router imports for..Routing
-import {Link} from 'react-router';
+import { Link } from "react-router";
 
 //Component's style
-import './Navbar.less';
+import "./Navbar.less";
 
 class NavBar extends Component {
-  constructor (props) {
-    super (props);
+  constructor(props) {
+    super(props);
   }
 
-  render () {
+  render() {
     return (
       <div className="NavBar">
         <ul>
+          <li>
+            <Link to="/data"><b>Data</b></Link>
+          </li>
           <li>
             <Link to="/hosts"><b>Hosts</b></Link>
           </li>
