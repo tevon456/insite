be2b9ff82df1bb83a0126b9f45a980cfa5397948
Jordan Jones <proxima.aust@gmail.com>
2017-07-29 19:20:41 -0500

    Added more routes

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 //Routes
 const initial = require("./get/initial.js");
+const stackedCharts = require("./get/stackedChart.js");
 
 router.get("/initial", initial);
+router.get("/charts/stacked/:id_agent/:stack_type", stackedCharts);
 
 module.exports = router;
