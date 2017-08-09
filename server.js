67eb8a36c040c57ee7fdfe8894e712131f51ec65
Jordan Jones <proxima.aust@gmail.com>
2017-08-09 13:56:39 -0500

    Fixed path error

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(require("morgan")("short"));
 app.use(express.static(join(__dirname, "/dist")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(join(__dirname, "index.html"));
 });
 
 // Register Routes
