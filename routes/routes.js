2c6e3b689cea0387b083c4327458277e931a8d75
Jordan Jones <proxima.aust@gmail.com>
2017-08-20 23:57:57 -0500

    Added datapoint and area chart Express routes

--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,13 @@ const router = express.Router();
 
 //Routes
 const initial = require("./get/initial.js");
+const datapoints = require("./get/fetchDatapoints.js");
+const areaCharts = require("./get/areaChart.js");
 const stackedCharts = require("./get/stackedChart.js");
 
 router.get("/initial", initial);
+router.get("/datapoints", datapoints);
+router.get("/charts/area/:datapointId", areaCharts);
 router.get("/charts/stacked/:id_agent/:stack_type", stackedCharts);
 
 module.exports = router;
