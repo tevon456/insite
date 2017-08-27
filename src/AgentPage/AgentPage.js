4e96cc50da2bbb11d6a93051e86b4fb1d11e9256
Jordan Jones <proxima.aust@gmail.com>
2017-08-27 18:48:32 -0500

    Agent Page completed

--- src/AgentPage/AgentPage.js
+++ src/AgentPage/AgentPage.js
@@ -5,41 +5,70 @@
  *  data: Data object from API request
  **/
 
-import React, {Component, Text} from 'react';
-import {get} from 'axios';
-import Navbar from '../Navbar/Navbar.js';
-import Footer from '../Dashboard/Footer/Footer.js';
+import React, { Component, Text } from "react";
+import { get } from "axios";
+
+import styled from "styled-components";
+import { Flex, Grid, Box } from "grid-styled";
+import Navbar from "../Navbar/Navbar.js";
+import Footer from "../Dashboard/Footer/Footer.js";
+import AgentPanel from "./AgentPanel/AgentPanel.js";
+
+const Container = styled(Box)`
+  max-width: 1024px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  box-shadow: 3px 3px 5px 3px #ccc;
+`;
 
 class AgentPage extends Component {
-  constructor () {
-    super ();
+  constructor(props) {
+    super(props);
     this.state = {
-      data: [],
+      agents: []
     };
   }
 
-  componentDidMount () {
-    get (this.props.route).then (function (response) {
+  fetchAgentPanels = agents => {
+    var agentPanels = [];
+    var key = 0;
+    console.log(agents);
+    for (var agent of agents) {
+      agentPanels.push(
+        <AgentPanel
+          key={key++}
+          agentName={agent.agent}
+          enabled={agent.enabled}
+          agentId={agent.idx_agent}
+          agentIdx={agent.idx_agentname}
+        />
+      );
+    }
+    return agentPanels;
+  };
+
+  componentDidMount() {
+    var _this = this;
+    get(this.props.agentRoute).then(function(response) {
       var data = response.data;
       //Times each time by 100 to prep for conversion
-      data.forEach (function (d) {
-        d.timestamp = d.timestamp * 1000;
-      });
-      _this.setState ({
-        data: response.data,
+      _this.setState({
+        agents: _this.fetchAgentPanels(data)
       });
     });
   }
 
-  render () {
+  render() {
     return (
       <div>
         <Navbar />
-        <h3>Agents</h3>
-
-        <h3>Agen name:</h3>
-        <h3>Agent ID</h3>
-        <h3>Last contact</h3>
+        <Container>
+          {this.state.agents}
+        </Container>
         <Footer />
       </div>
     );
@@ -48,7 +77,7 @@ class AgentPage extends Component {
 
 //Default properties
 AgentPage.defaultProps = {
-  route: 'agents',
+  agentRoute: "/agents"
 };
 
 //Exports class to Global namespace
