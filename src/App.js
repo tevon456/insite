33a64ae8475e764136bbda17a144fe82eb8b6794
Jordan Jones <proxima.aust@gmail.com>
2019-05-29 13:27:29 -0500

    Added insite-ui library

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button>New Message</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
