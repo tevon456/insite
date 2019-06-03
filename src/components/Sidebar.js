2e89982704ecb9e868f8233e082276282d862478
Jordan Jones <proxima.aust@gmail.com>
2019-06-03 15:53:06 -0500

    Move sidebar from insite-ui

---
+++ src/components/Sidebar.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { primary, secondary, textForeground } from 'insite-ui'
+// eslint-disable-next-line
+import styled from 'styled-components/macro'
+
+class Sidebar extends React.Component {
+  render() {
+    return (
+      <div
+        css={`
+          position: sticky;
+          width: 300px;
+          height: 100vh;
+          color: ${textForeground};
+          border: none;
+          background: ${secondary};
+          &:focus {
+            border: none;
+            outline: none;
+          }
+        `}
+      >
+        <div
+          css={`
+            background: ${primary};
+            padding: 3px;
+            text-align: center;
+          `}
+        >
+          <h1
+            css={`
+              font-family: Pridi;
+              font-weight: 400;
+              font-size: 40px;
+              letter-spacing: 1px;
+            `}
+          >
+            insite
+          </h1>
+        </div>
+        <div
+          css={`
+            padding: 20px;
+            margin: 0 auto;
+            text-align: center;
+            font-family: Lato, san-serif;
+          `}
+        >
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Sidebar
